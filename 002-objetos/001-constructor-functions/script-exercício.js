a9b391f6a03ac34896547955fb094b72b3e6c55f
Rodrigo Hickenbick <rodrigo.hickenbick@gmail.com>
2022-06-16 22:53:41 +0100

    update

---
+++ 002-objetos/001-constructor-functions/script-exercício.js
@@ -0,0 +1,47 @@
+// Transforme o objeto abaixo em uma Constructor Function
+// const pessoa = {
+//   nome: 'Nome pessoa',
+//   idade: 0,
+//   andar() {
+//     console.log(this.nome + ' andou');
+//   }
+// }
+
+// ==================================
+
+function Pessoa(nome, idade) {
+  this.nome = nome;
+  this.idade = idade;
+  this.andar = function() {
+    console.log(nome + ' andou')
+  }
+}
+
+// Crie 3 pessoas, João - 20 anos,
+// Maria - 25 anos, Bruno - 15 anos
+
+const joao = new Pessoa('João', '20')
+const maria = new Pessoa('Maria', '25')
+const bruno = new Pessoa('Bruno', '15')
+
+console.log(joao.andar())
+console.log(maria.andar())
+console.log(bruno.andar())
+
+
+// Crie uma Constructor Function (Dom) para manipulação
+// de listas de elementos do dom. Deve conter as seguintes
+// propriedades e métodos:
+//
+// elements, retorna NodeList com os elementos selecionados
+// addClass(classe), adiciona a classe a todos os elementos
+// removeClass(classe), remove a classe a todos os elementos
+
+function Dom(seletor) {
+  this.elementList = document.querySelectorAll(seletor)
+}
+
+const li = new Dom('li')
+console.log(li.element())
+
+
--- 002-objetos/001-constructor-functions/script.js
+++ 002-objetos/001-constructor-functions/script.js
@@ -1,18 +1,71 @@
-function Carro(marcaAtribuida, anoAtribuido, precoAtribuido) {
-  this.marca = marcaAtribuida;
-  this.ano = anoAtribuido
-  this.preco = precoAtribuido;
-}
+// function Carro(marcaAtribuida, anoAtribuido, precoAtribuido) {
+//   this.marca = marcaAtribuida;
+//   this.ano = anoAtribuido
+//   this.preco = precoAtribuido;
+// }
 
-const honda = new Carro('Honda', '1995', 'R$ 2000,00');
-const fiat = new Carro('Fiat', '2005', 'R$ 3000,00' );
+// const honda = new Carro('Honda', '1995', 'R$ 2000,00');
+// const fiat = new Carro('Fiat', '2005', 'R$ 3000,00' );
 
-function Carro2(marca, precoInicial) {
-  const taxa = 1.2;
-  const precoFinal = precoInicial * taxa;
-  this.marca = marca;
-  this.preco = precoFinal;
-}
+// function Carro2(marca, precoInicial) {
+//   this.taxa = 1.2;
+//   this.precoFinal = precoInicial * this.taxa;
+//   this.marca = marca;
+// }
+// const mazda = new Carro2('Mazda', 1000)
+// console.log(mazda)
 
-const mazda = new Carro2('mazda', 5000)
-console.log(mazda)
+// const hundai = new Carro2('Hundai', 2000)
+// console.log(hundai)
+
+// const Dom = {
+//   seletor: 'li',
+//   element() {
+//     return document.querySelector(this.seletor)
+//   },
+//   ativar() {
+//     this.element().classList.add('ativar')
+//   }
+// }
+
+// function Dom(seletor) {
+//   this.element = function() {
+//     return document.querySelector(seletor);
+//   }
+//   this.ativar = function() {
+//     this.element().classList.add('ativar')
+//   }
+// }
+
+// const li = new Dom('li')
+// li.ativar()
+// const ul = new Dom('ul')
+// ul.ativar()
+
+
+// function Dom(seletor) {
+//   this.element = function() {
+//     return document.querySelector(seletor)
+//   }
+//   this.ativar = function() {
+//     this.element().classList.add('ativar')
+//   }
+// }
+
+// const li = new Dom('li')
+// li.ativar()
+
+// function Dom(seletor) {
+//   this.element = function() {
+//     return document.querySelector(seletor)
+//   }
+//   this.ativar = function() {
+//     this.element().classList.add('ativar')
+//   }
+// }
+
+// const li = new Dom('li')
+// li.ativar()
+// const ul = new Dom('ul')
+// ul.ativar()
+
