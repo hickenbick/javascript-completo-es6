86c55b86775b537d10176ca75d4d47169836c557
Rodrigo Hickenbick <rodrigo.hickenbick@gmail.com>
2022-06-18 15:04:58 +0100

    update

--- 002-objetos/001-constructor-functions/script-exercício.js
+++ 002-objetos/001-constructor-functions/script-exercício.js
@@ -12,22 +12,21 @@
 function Pessoa(nome, idade) {
   this.nome = nome;
   this.idade = idade;
-  this.andar = function() {
-    console.log(nome + ' andou')
-  }
+  this.andar = function () {
+    console.log(nome + " andou");
+  };
 }
 
 // Crie 3 pessoas, João - 20 anos,
 // Maria - 25 anos, Bruno - 15 anos
 
-const joao = new Pessoa('João', '20')
-const maria = new Pessoa('Maria', '25')
-const bruno = new Pessoa('Bruno', '15')
+const joao = new Pessoa("João", "20");
+const maria = new Pessoa("Maria", "25");
+const bruno = new Pessoa("Bruno", "15");
 
-console.log(joao.andar())
-console.log(maria.andar())
-console.log(bruno.andar())
-
+console.log(joao.andar());
+console.log(maria.andar());
+console.log(bruno.andar());
 
 // Crie uma Constructor Function (Dom) para manipulação
 // de listas de elementos do dom. Deve conter as seguintes
@@ -37,11 +36,45 @@ console.log(bruno.andar())
 // addClass(classe), adiciona a classe a todos os elementos
 // removeClass(classe), remove a classe a todos os elementos
 
-function Dom(seletor) {
-  this.elementList = document.querySelectorAll(seletor)
-}
+// function Dom(seletor) {
+//   const elementList = document.querySelectorAll(seletor);
+//   this.elements = elementList;
+// }
+// const li = new Dom('li')
+// console.log(li.elements)
 
+// const ul = new Dom('ul')
+// console.log(ul.elements)
+
+// function Dom(seletor) {
+//   elementList = document.querySelectorAll(seletor);
+//   this.elements =  elementList;
+//   this.addClass = function(classe) {
+//     elementList.forEach(function(e) {
+//       e.classList.add(classe)
+//     })
+//   }
+// }
+
+// const li = new Dom('li')
+// console.log(li.addClass('ativo'))
+
+function Dom(seletor) {
+  const elementList = document.querySelectorAll(seletor);
+  this.elements = elementList;
+  this.addClass = function (classe) {
+    elementList.forEach(function (e) {
+      e.classList.add(classe);
+    });
+  };
+  this.removeClass = function(classe) {
+    elementList.forEach((e) => {
+      e.classList.remove(classe)
+    })
+  }
+}
 const li = new Dom('li')
-console.log(li.element())
+// li.addClass('ativo')
+// li.removeClass('ativo')
 
 
---
+++ 002-objetos/002-prototype/script.js
@@ -0,0 +1,38 @@
+// function Pessoa(nome, idade) {
+//   this.nome = nome;
+//   this.idade = idade;
+// }
+
+// const rodrigo = new Pessoa('Rodrigo', 29)
+
+// console.log(Pessoa.prototype)
+// console.log(rodrigo.prototype)
+
+// function Pessoa(nome, idade){
+//   this.nome = nome;
+//   this.idade = idade;
+//   this.andar = function() {
+//     return 'andou pelo objeto'
+//   }
+// }
+
+// Pessoa.prototype.andar = function() {
+//   return this.nome + ' andou'
+// }
+
+// const rodrigo = new Pessoa('Rodrigo', 29)
+// console.log(rodrigo.andar())
+
+// console.log(rodrigo.andar())
+
+// const pais = 'Brasil'
+// const cidade = new String('Curitiba')
+
+const animais = ['Cachorro', 'Gato', 'Pássaro', 'Tartaruga']
+
+const lista = document.querySelectorAll('li')
+
+
+
+
+
